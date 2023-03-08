2fc1c5de8e1dbb891d2c116e4c126c5673a10a77
Yasin ilkalp Arabacı <yasinilkalp_24@hotmail.com>
2023-03-08 16:11:04 +0300

    update vercel

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const playwright = require('playwright');
 const Redis = require("ioredis");
+const express = require("express");
+var cors = require('cors');
 
-(async () => {
-    const browser = await playwright.chromium.launch({
-        headless: false
-    });
-    const page = await browser.newPage();
+const app = express();
+app.use(cors());
 
+
+const connectRedis = () => {
     let redisConfig = {
         uName: process.env.UPSTASH_REDIS_USERNAME,
         pwd: process.env.UPSTASH_REDIS_PASSWORD,
@@ -15,11 +16,32 @@ const Redis = require("ioredis");
         port: process.env.UPSTASH_REDIS_PORT
     }
     let client = new Redis(`rediss://${redisConfig.uName}:${redisConfig.pwd}@${redisConfig.host}:${redisConfig.port}/0`);
+    return client;
+};
 
-    await onLogin(page);
+const run = async () => {
+    const browser = await playwright.chromium.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+
+    var isLogin = false;
+    let tryLoginCount = 0;
+    while (!isLogin) {
+        console.log('------------ Login olmaya çalışıyoruz.');
+        await onLogin(page);
+
+        let url = await page.url();
+        console.log('------------ Girdik mi?', url);
+        isLogin = url == "https://partner.trendyol.com/dashboard";
+        tryLoginCount++;
+        if (tryLoginCount === 4) return;
+    }
+    console.log('------------ Login olduk devam ediyoruz.');
 
     await page.goto("https://partner.trendyol.com/incentive");
 
+    console.log('------------ Sayfalama 50lik datalara çekiyoruz..');
     await page.selectOption("div.change-size select", "50");
 
     await delay(1000);
@@ -27,31 +49,42 @@ const Redis = require("ioredis");
     var dataControl = true;
     var tableData = [];
     while (dataControl) {
+        console.log('------------ Verileri getirmeye başladık');
         await readTable(page, tableData);
         dataControl = await nextPage(page);
     }
 
-    client.set("categories", JSON.stringify(tableData));
+    console.log('------------ Verileri aldık. Redise kaydediyoruz.');
 
+    let client = connectRedis();
+    client.del('categories');
+    console.log(JSON.stringify(tableData.filter(x => x.mainCategory === 'Süpermarket')));
+    client.set("categories", JSON.stringify(tableData.filter(x => x.mainCategory === 'Süpermarket')));
+
+    await page.close();
     await browser.close();
 
-})();
+};
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 const onLogin = async (page) => {
-    await page.goto('https://partner.trendyol.com/account/login');
+    await page.goto('https://partner.trendyol.com/account/login', { waitUntil: 'domcontentloaded' });
 
+    console.log('------------ E-posta adresini giriyoruz.');
     let emailInput = page.locator("div.email-phone input");
     await emailInput.fill(process.env.TRENDYOL_USERNAME);
 
+    console.log('------------ Şifreyi giriyoruz.');
     let pwdInput = page.locator("div.password input");
     await pwdInput.fill(process.env.TRENDYOL_PASSWORD);
 
+    console.log('------------ Giriş butonuna bastık.');
     let loginBtn = page.locator("button[type=submit]");
     await loginBtn.click();
 
     await delay(5000);
+
 };
 
 const readTable = async (page, tableData) => {
@@ -79,3 +112,22 @@ const nextPage = async (page) => {
         return false;
     }
 };
+
+app.get("/", async (req, res, next) => {
+    let client = connectRedis();
+    let data = await client.get('categories');
+    res.json(data);
+});
+
+app.get("/run", async (req, res, next) => {
+
+    run();
+    res.json('başladı')
+});
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+    console.log(`Server running on ${port}, http://localhost:${port}`)
+);
